1501e07fbaadbaeabbf17681e70cbb271fffcc24
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-01-24 01:41:11 -0200

    Failed try at performance enhancement

--- src/mmch.py
+++ src/mmch.py
@@ -1,72 +1,85 @@
 from fasta.Fasta import read_fasta
 import networkx as nx
+import time
+import threading
 
+#http://code.activestate.com/recipes/286222/
 import os
 
 _proc_status = '/proc/%d/status' % os.getpid()
 
 _scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
+		  'KB': 1024.0, 'MB': 1024.0*1024.0}
 
 def _VmB(VmKey):
-    '''Private.
-    '''
-    global _proc_status, _scale
-     # get pseudo file  /proc/<pid>/status
-    try:
-        t = open(_proc_status)
-        v = t.read()
-        t.close()
-    except:
-        return 0.0  # non-Linux?
-     # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
-    i = v.index(VmKey)
-    v = v[i:].split(None, 3)  # whitespace
-    if len(v) < 3:
-        return 0.0  # invalid format?
-     # convert Vm value to bytes
-    return float(v[1]) * _scale[v[2]]
+	'''Private.
+	'''
+	global _proc_status, _scale
+	# get pseudo file  /proc/<pid>/status
+	try:
+		t = open(_proc_status)
+		v = t.read()
+		t.close()
+	except:
+		return 0.0  # non-Linux?
+	# get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+	i = v.index(VmKey)
+	v = v[i:].split(None, 3)  # whitespace
+	if len(v) < 3:
+		return 0.0  # invalid format?
+	# convert Vm value to bytes
+	return float(v[1]) * _scale[v[2]]
 
 
 def memory(since=0.0):
-    '''Return memory usage in bytes.
-    '''
-    return _VmB('VmSize:') - since
+	'''Return memory usage in bytes.
+	'''
+	return _VmB('VmSize:') - since
 
 
 def resident(since=0.0):
-    '''Return resident memory usage in bytes.
-    '''
-    return _VmB('VmRSS:') - since
+	'''Return resident memory usage in bytes.
+	'''
+	return _VmB('VmRSS:') - since
 
 
 def stacksize(since=0.0):
-    '''Return stack size in bytes.
-    '''
-    return _VmB('VmStk:') - since
+	'''Return stack size in bytes.
+	'''
+	return _VmB('VmStk:') - since
+
+class Counter():
+
+	def __init__(self, count):
+		self.lock = threading.Lock()
+		self.count = count
+		
+	def increment(self):
+		with self.lock:
+			self.count += 1
 
 # I'm sure there's some well-tested algorithm for that... but I just couldn't find it... then...
-def search(matchings, g, idx, stack):
-	node = g.nodes()[idx]
-	for right in g.neighbors(node):
-		edge = (node, right)
-		if False == matchings[-1].has_node(right):
-			stack.append(edge)
-			matchings[-1].add_edge(*edge)
+def search(g, idx, nodes, counter):
+	left = g.nodes()[idx]
+	for right in g.neighbors(left):
+		if right not in nodes: # if False == matchings[-1].has_node(right):
+			nodes.append(left)
+			nodes.append(right)
+			# matchings[-1].add_edge(*edge) # adding a part of the matching
 		if (idx + 1 < len(g.nodes())):
-			search(matchings, g, idx +1, stack)
-			if len(stack) > 0 and stack[-1] == edge:
-				stack.pop()
-				for i in xrange(0, len(stack)):
-					edge = stack.pop()
-					matchings[-1].add_edge(*edge)
+			search(g, idx +1, nodes, counter)
+					# matchings[-1].add_edge(*edge) # adding a part of the matching
 		else: # last node
-			matchings.append(nx.Graph())
+			# matchings.append(nx.Graph())
+			if len(nodes) > 0: # we've found a matching...
+				counter.increment()
+			del nodes[:]
 
 if __name__ == '__main__':
+	start_time = time.time()
 	data = '''
 >Rosalind_0976
-AUGCUUCA
+CUAUCAUCGCGGUCACCUCG
 '''
 
 	seqs = read_fasta(data)
@@ -99,11 +112,23 @@ AUGCUUCA
 	#print G.edges()
 
 	m2 = memory(m1)
-	matchings = list()
-	matchings.append(nx.Graph())
 
-	stack = list()
-	search(matchings, G, 0, stack)
+	counter = Counter(0)
+	
+	left = G.nodes()[0]
+	threads = list()
+	for right in G.neighbors(left):
+		nodes = list()
+		nodes.append(left)
+		nodes.append(right)
+		t = threading.Thread(target=search, args = (G, 1, nodes, counter))
+		t.daemon = True
+		t.start()
+		threads.append(t)
+		#search(G, 1, nodes, counter)
+
+	for t in threads:
+		t.join()
 
 	m3 = memory(m2)
 
@@ -113,6 +138,7 @@ AUGCUUCA
 #		print "MATCHING!"
 #		print g.edges()
 
-	print len(matchings)
+	print counter.count
 
 #	print list(itertools.combinations(sequence, 6))
+	print("--- %s seconds ---" % str(time.time() - start_time))
